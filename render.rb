250d72c76ae8793d5c93c873593cea30712ef497
Sydney Young <sydney.young@yale.edu>
2018-05-09 03:06:41 -0400

    shadows

---
+++ render.rb
@@ -0,0 +1,17 @@
+require 'erb'
+
+def hash_to_style(top:, left:)
+  "top: #{top}%; left: #{left}%"
+end 
+
+def render_shape(klass:, top:, left:)
+  "<div class='#{klass}' style='top: #{top}%; left:#{left}%'></div>"
+end
+
+def erase(width: 5, height: 5, top:, left:)
+  "<div class='eraser' style='width: #{width}px; height: #{height}px; "\
+    "top: #{top}%; left:#{left}%'></div>"
+end
+
+f = File.read("index.html.erb")
+puts ERB.new(f).result
